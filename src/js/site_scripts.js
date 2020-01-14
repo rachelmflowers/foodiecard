4aad3eef3500fbb89694bd2c95bbc034bde3c5e4
Rachel Flowers <rachel@appicstudios.com>
2020-01-14 00:12:12 -0500

    rest of the client requested changes

--- src/js/site_scripts.js
+++ src/js/site_scripts.js
@@ -5,8 +5,8 @@ function ScrollPoint(elem) {
             root: null,
             threshold: 0.25
         };
-        
-    if (typeof(scrollElem) != 'undefined' && scrollElem != null) {
+
+    if (typeof (scrollElem) != 'undefined' && scrollElem != null) {
         console.log('there is a scroll elem');
         if (scrollElem.hasAttribute("data-scrollMargin")) {
             var scrollMargin = scrollElem.getAttribute('data-scrollMargin');
@@ -18,7 +18,7 @@ function ScrollPoint(elem) {
         }
 
         function observerCallback(entries, observer) {
-            entries.forEach(entry => { 
+            entries.forEach(entry => {
 
                 if (entry.intersectionRatio > 0) {
                     if (entries[0].boundingClientRect.y < 100) {
@@ -44,78 +44,238 @@ function ScrollPoint(elem) {
     }
 }
 
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip();
+// GOOGLE MAPS
+// create a load function to load scripts
+const load = async (script) => {
+    return new Promise(async ready => {
+        for (i = 0; i < document.scripts.length; i++) {
+            if (document.scripts[i].src == script) {
+                return r()
+            }
+        }
+        let elem = document.createElement('script');
+        elem.src = script;
+        document.body.appendChild(elem)
+        elem.onload = ready;
+    })
+}
 
-    // Selectr custom select boxes
-    var restaurantCity = document.getElementById("restaurantCity"),
-        restaurantZip = document.getElementById("restaurantZip"),
-        shippingState = document.getElementById("shippingState"),
-        cusineType = document.getElementById("shippingState");
+// create google map function
+// - default zoom is 15 (street level)
+// - default map markers are not shown
+// - default: default map UI is not disabled
+const gmap = async (elem) => {
+    return new Promise(async ready => {
+        let latlng = elem.getAttribute('data-mapLatLng').split(',');
+        let Lat = parseFloat(latlng[0]);
+        let Lng = parseFloat(latlng[1]);
+        let location = { lat: Lat, lng: Lng };
+        let zoom = elem.hasAttribute("data-mapZoom") ? parseFloat(elem.getAttribute("data-mapZoom")) : 15;
+        let mapMarker = elem.hasAttribute("data-mapMarker") ? true : false;
+        let disableMapUI = elem.hasAttribute("data-disableMapUI") ? true : false;
 
-    if (typeof(restaurantCity) != 'undefined' && restaurantCity != null) {
-        new Selectr('#restaurantCity', {
-            defaultSelected: true
+        await load('https://maps.googleapis.com/maps/api/js?key=AIzaSyDLBAYbQnUvDYI8eANtjvfSxbJx11SpGts&v=3');
+
+        let map = new google.maps.Map(elem, {
+            center: location,
+            zoom: zoom,
+            disableDefaultUI: disableMapUI
         });
-    }
 
-    if (typeof(restaurantZip) != 'undefined' && restaurantZip != null) {
-        new Selectr('#restaurantZip', {
-            defaultSelected: true
+        if (mapMarker) {
+            // The marker, positioned at location
+            let marker = new google.maps.Marker({ position: location, map: map });
+        }
+
+        ready(map)
+    })
+}
+
+// find maps and load
+(async () => {
+    let maps = document.querySelectorAll("div[data-controls='gmap']");
+    for (var i = 0; i < maps.length; i++) {
+        await gmap(maps[i]);
+    }
+})();
+
+
+(function ($) {
+
+    // Begin FIX function for focusable elements for accessibility
+    jQuery.extend(jQuery.expr[':'], {
+        focusable: function (el, index, selector) {
+            var $element = $(el);
+            return $element.is(':input:enabled, a[href], area[href], object, [tabindex]') && !$element.is(':hidden');
+        }
+    });
+
+    function focusOnElement($element) {
+        if (!$element.length) {
+            return;
+        }
+        if (!$element.is(':focusable')) {
+            // add tabindex to make focusable and remove again
+            $element.attr('tabindex', -1).on('blur focusout', function () {
+                $(this).removeAttr('tabindex');
+            });
+        }
+        $element.focus();
+    }
+    // END fix for focusable elements
+
+    $(document).ready(function () {
+        // Begin focusable fix for accessibility
+        // if there is a '#' in the URL (someone linking directly to a page with an anchor)
+        if (document.location.hash) {
+            focusOnElement($(document.location.hash));
+        }
+
+        // if the hash has been changed (activation of an in-page link)
+        $(window).bind('hashchange', function () {
+            var hash = "#" + window.location.hash.replace(/^#/, '');
+            focusOnElement($(hash));
         });
-    }
+        // END focusable fix
 
-    if (typeof(restaurantCity) != 'undefined' && restaurantCity != null) {
-        new Selectr('#shippingState', {
-            placeholder: "State"
+        $('[data-toggle="tooltip"]').tooltip();
+
+        // How it works Scroll observer
+        new ScrollPoint('#step-1');
+        new ScrollPoint('#step-2');
+
+
+        // CHECKOUT PAGE
+        
+        $('.showRegistration').on('click', function() {
+            $('#loginForm_container').hide();
+            $('#registerForm_container').addClass('show');
         });
-    }
-
-    if (typeof(cuisineType) != 'undefined' && cuisineType != null) {
-        new Selectr('#cuisineType', {
-            multiple: true,
-            defaultSelected: true
+        
+        $('.showLogin').on('click', function() {
+            $('#registerForm_container').hide();
+            $('#loginForm_container').addClass('show');
         });
-    }
 
-    // Flickity Scrollers
-    var featArticles = document.getElementById('featuredArticlesSlider'),
-        testimonials = document.querySelector('.testimonial-slider'),
-        featRestaurants = document.getElementById('featRestSlider');
-
-    if (typeof(featArticles) != 'undefined' && featArticles != null) {
-        new Flickity( featArticles, {
-            wrapAround: true
+        $('.showCheckout').on('click', function() {
+            $('#loginForm_container').hide();
+            $('#registerForm_container').hide();
+            $('#checkout').addClass('show');
         });
-    }
 
-    if (typeof(featRestaurants) != 'undefined' && featRestaurants != null) {
-        new Flickity( featRestaurants, {
-            cellAlign: 'left',
-            contain: true,
-            groupCells: '80%'
-        });
-    }
+        $('.add-card').on('click', function() {
+            var newCard = $(this).next('.input-group').clone();
+            
+            $(newCard).insertBefore(this);
+        })
 
-    if (typeof(testimonials) != 'undefined' && testimonials != null) {
-        new Flickity( testimonials, {
-            cellAlign: 'left',
-            fade: true,
-            autoPlay: 3000,
-            wrapAround: true
-        });
-    }
+        $('.emailMessage').on('change', function() {
+            if ( $(this).is(':checked') ) {
+                $(this).parent().next('.messageEmail').addClass('show');
+            } else {
+                $(this).parent().next('.messageEmail').removeClass('show');
+            }
+        })
 
-    // How it works Scroll observer
-    new ScrollPoint('#step-1');
-    new ScrollPoint('#step-2');
+        $('.add-gift').on('click', function() {
+            var newGift = $(this).next('.additional-recipient').clone();
+            
+            $(newGift).insertBefore(this);
+        })
 
-    // GOOGLE MAPS
-    var googleMap;
-    function initMap() {
-        googleMap = new google.maps.Map(document.getElementById('rest-name-google-map'), {
-            center: {lat: 40.875078, lng: -73.170679},
-            zoom: 11
-        });
-    }
-});
+        // Selectr custom select boxes
+        var restaurantCity = document.getElementById("restaurantCity"),
+            restaurantZip = document.getElementById("restaurantZip"),
+            shippingState = document.getElementById("shippingState"),
+            cusineType = document.getElementById("cusineType");
+
+        if (typeof (restaurantCity) != 'undefined' && restaurantCity != null) {
+            new Selectr('#restaurantCity', {
+                defaultSelected: true
+            });
+        }
+
+        if (typeof (restaurantZip) != 'undefined' && restaurantZip != null) {
+            new Selectr('#restaurantZip', {
+                defaultSelected: true
+            });
+        }
+
+        // if (typeof (shippingState) != 'undefined' && shippingState != null) {
+        //     new Selectr('#shippingState', {
+        //         placeholder: "State"
+        //     });
+        // }
+
+        if (typeof (cusineType) != 'undefined' && cusineType != null) {
+            new Selectr('#cusineType', {
+                multiple: true,
+                defaultSelected: true
+            });
+        }
+
+        // Flickity Scrollers
+        var featArticles = document.getElementById('featuredArticlesSlider'),
+            testimonials = document.querySelector('.testimonial-slider'),
+            featRestaurants = document.getElementById('featRestSlider'),
+            imageSlider = document.getElementById('imageSlider'),
+            modalImageSlider = document.getElementById('modalImageSlider');
+
+        if (typeof (featArticles) != 'undefined' && featArticles != null) {
+            new Flickity(featArticles, {
+                wrapAround: true
+            });
+        }
+
+        if (typeof (testimonials) != 'undefined' && testimonials != null) {
+            new Flickity(testimonials, {
+                cellAlign: 'left',
+                fade: true,
+                autoPlay: true,
+                wrapAround: true
+            });
+        }
+
+        if (typeof (featRestaurants) != 'undefined' && featRestaurants != null) {
+            new Flickity(featRestaurants, {
+                cellAlign: 'left',
+                contain: true,
+                groupCells: '80%'
+            });
+        }
+
+        if (typeof (imageSlider) != 'undefined' && imageSlider != null) {
+            new Flickity(imageSlider, {
+                cellAlign: 'left',
+                autoPlay: 4500,
+                pageDots: false,
+                wrapAround: true,
+            });
+        }
+
+        if (typeof (modalImageSlider) != 'undefined' && modalImageSlider != null) {
+
+            var modalSlider = new Flickity(modalImageSlider, {
+                cellAlign: 'center',
+                imagesLoaded: true,
+                percentPosition: false,
+                setGallerySize: false,
+                pageDots: false,
+                contain: true,
+            });
+
+            $('#imageSlider').on('click', '.image-container', function() {
+                // open modal
+                $('#imageSliderModal').modal('show');
+
+                // show correct slide
+                var index = $(this).index();
+                modalSlider.select( index, true, true )
+            });
+
+            $('#imageSliderModal').on( 'shown.bs.modal', function() {
+                modalSlider.resize()
+            });
+        }
+    });
+})(jQuery);
